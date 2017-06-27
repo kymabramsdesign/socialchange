f3900cc281d6f7dfba6076042f3905f69971eeea
Brandon Trumfio <brandon@kad.com>
2017-06-27 13:52:26 -0500

    homepage work

--- wp-content/themes/social-change/front-page.php
+++ wp-content/themes/social-change/front-page.php
@@ -51,6 +51,44 @@ get_header(); ?>
 
     <?php endif; ?>
 
+    <div class="change-agents">
+      <h1>Change Agents</h1>
+
+      <div class="article-container">
+
+      <?php if( have_rows('change_agents') ): ?>
+
+
+
+        <div class="article-list">
+          <?php while( have_rows('change_agents') ): the_row();
+
+            // vars
+            $agent = get_sub_field('agent'); ?>
+
+                <div class="item">
+
+                  <a href="<?php the_permalink($agent); ?>">
+                    <?php echo get_the_post_thumbnail($agent, 'article-listing'); ?></a>
+
+                  <h5>
+                    <?php $post_term = get_the_term_list($agent, 'post_tag' );
+                    print_r($post_term); ?>
+                  </h5>
+
+                  <a href="<?php the_permalink($agent); ?>" class="title"><?php the_title($agent); ?></a>
+
+                    <p><?php $excerpt = wp_trim_words( get_field('article_content' ), $num_words = 18, $more = '...' ); echo $excerpt ?></p>
+               </div><!-- item -->
+          <?php endwhile; ?>
+
+          </div><!-- .article-list -->
+
+        <?php endif; ?>
+        </div><!-- .article-container -->
+
+    </div>
+
 
     </main><!-- #main -->
   </div><!-- #primary -->
